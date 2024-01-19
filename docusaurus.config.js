6f00706f3e1bfed75d52127153e56c0ee657ec15
IfcAddict <evelio@ifcaddict.com>
2024-01-19 16:37:07 +0100

    Modificada configuración docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://IfcAddict.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/PEB/',
   organizationName: 'IfcAddict', // Usually your GitHub org/user name.
   projectName: 'PEB', // Usually your repo name.
 
